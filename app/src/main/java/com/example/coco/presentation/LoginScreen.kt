447bd8e5a23b474272be79e09be514cc15600fc6
Jose Ricardo <joseanakin@outlook.es>
2024-02-10 19:31:10 -0600

    LoginScreen finished

--- app/src/main/java/com/example/coco/presentation/LoginScreen.kt
+++ app/src/main/java/com/example/coco/presentation/LoginScreen.kt
@@ -1,63 +1,226 @@
 package com.example.coco.presentation
 
+import android.widget.Space
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.coco.R
 import com.example.coco.ui.theme.buttonHome
+import com.example.coco.ui.theme.fredoka
+import com.example.coco.ui.theme.surfaceHome
 
 @Composable
 fun LoginScreen() {
     Box {
-        Footer( Modifier.align( Alignment.BottomCenter ))
+        Footer(Modifier.align(Alignment.BottomCenter))
 
         Image(
-            modifier = Modifier.align( Alignment.BottomCenter ).fillMaxWidth(),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth(),
             painter = painterResource(id = R.drawable.login_background),
-            alpha = 0.5f,
+            alpha = 0.2f,
             contentDescription = "background",
             contentScale = ContentScale.FillHeight,
         )
 
 
-        Body(mod = Modifier.align( Alignment.Center ))
+        Body(mod = Modifier.align(Alignment.Center))
     }
 }
+
 @Composable
-fun Body( mod: Modifier ) {
+fun Body(mod: Modifier) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
     Column(
-        modifier = mod
+        modifier = mod.padding( 32.dp )
     ) {
-        Image(painter = painterResource(id = R.drawable.ic_launcher), contentDescription = "Coco Icon")
+        Image(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(200.dp),
+            painter = painterResource(id = R.drawable.ic_launcher),
+            contentDescription = "Coco Icon"
+        )
 
-        OutlinedTextField(value = "Email Address", onValueChange = { } )
-        OutlinedTextField(value = "Password", onValueChange = {} )
+        MyTextField(value = email, placeholder = R.string.placeholderEmail, Icons.Filled.Email, onValueChange = {
+            email = it
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        MyTextField(value = password, placeholder = R.string.placeholderPass, imageVector = Icons.Filled.Lock, onValueChange = {
+            password = it
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton(
+            modifier = Modifier.align( Alignment.End ),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                text = stringResource(id = R.string.forget),
+                fontFamily = fredoka,
+                fontWeight = FontWeight.Medium,
+                color = buttonHome,
+                fontSize = 16.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .align(Alignment.CenterHorizontally),
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(10.dp ),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = buttonHome
+            )
+        ) {
 
-        Text(text = "Forget Password?")
+            Text(
+                text = stringResource(id = R.string.LOGIN),
+                fontFamily = fredoka,
+                color = Color.Black,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 22.sp
+            )
+        }
 
+        // Divider
+        Spacer(modifier = Modifier.height(16.dp))
+        MyDivider()
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column {
+            ButtonLogWith(
+                text = stringResource(id = R.string.loginGoogle),
+                drawable = R.drawable.ic_google
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            ButtonLogWith(
+                text = stringResource(id = R.string.loginFacebook),
+                drawable = R.drawable.ic_facebook
+            )
+            Spacer(modifier = Modifier.height(6.dp))
+            ButtonLogWith(
+                text = stringResource(id = R.string.loginApple),
+                drawable = R.drawable.ic_apple
+            )
+        }
     }
 }
 @Composable
-fun Footer( mod: Modifier = Modifier ) {
+fun MyDivider() {
+    Row( verticalAlignment = Alignment.CenterVertically) {
+        Divider(
+            Modifier
+                .padding(end = 8.dp)
+                .weight(1f))
+        Text(text = stringResource(id = R.string.orConnect), color = Color.Gray, fontFamily = fredoka, fontWeight = FontWeight.Medium)
+        Divider(
+            Modifier
+                .padding(start = 8.dp)
+                .weight(1f))
+    }
+}
+@Composable
+fun MyTextField(value: String, placeholder: Int, imageVector: ImageVector, onValueChange: (String) -> Unit) {
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp)),
+        value = value,
+        onValueChange = { onValueChange(it) },
+        leadingIcon = {
+            Icon(imageVector = imageVector, contentDescription = "Email")
+        },
+        placeholder = {
+            Text(text = stringResource(id = placeholder), fontFamily = fredoka, fontWeight = FontWeight.Medium)
+                      },
+        colors = TextFieldDefaults.colors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedPlaceholderColor = Color.Gray,
+            unfocusedLeadingIconColor = Color.Gray
+        )
+    )
+}
+
+@Composable
+fun ButtonLogWith(text: String, @DrawableRes drawable: Int) {
+    Button(
+        onClick = { },
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = buttonHome
+        )
+    )
+    {
+        Spacer(modifier = Modifier.width(16.dp))
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = text,
+            modifier = Modifier.size(24.dp)
+        )
+        Text(text = text, modifier = Modifier.weight(1f).padding(start = 16.dp),
+            fontFamily = fredoka,
+            fontWeight = FontWeight.Normal,
+            color = Color.Black,
+            fontSize = 20.sp)
+    }
+}
+
+@Composable
+fun Footer(mod: Modifier = Modifier) {
     Text(
         modifier = mod
             .fillMaxWidth()
             .background(buttonHome)
             .padding(top = 40.dp, bottom = 16.dp),
-        text = "© All Rights Reserverd to Pixel Posse - 2022",
+        text = stringResource(id = R.string.footerText),
         textAlign = TextAlign.Center,
         fontSize = 10.sp,
     )
